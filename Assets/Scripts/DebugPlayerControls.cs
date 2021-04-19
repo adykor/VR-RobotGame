697b79d4351420b90d2e5cfdd08444a1c6a39173
Kyerstin O'Neal <kcrocker1@cnm.edu>
2021-04-19 16:17:43 -0600

    added ground to walkable layer

--- Assets/Scripts/DebugPlayerControls.cs
+++ Assets/Scripts/DebugPlayerControls.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class DebugPlayerControls : MonoBehaviour
 {
+    private NavMeshAgent navAgent;
+
     void Start()
     {
-        
+        // Store the nav mesh agent 
+        navAgent = GetComponent<NavMeshAgent>();
     }
     void Update()
     {
         // If the left mouse button has been pressed
-            // Convert the mouse screen position to a world ray
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+        // Convert the mouse screen position to a world ray
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
             // Raycast into the world 
-                // Set the player's destinatoin to the point we clicked
+            if(Physics.Raycast(ray, out var hit))
+        {
+            // If the object that was clicked is "walkable" 
+            // Set the player's destinatoin to the point we clicked
+
+        }
+
     }
 }
