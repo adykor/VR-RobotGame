d1f94e181e70c2279c385fc9dcc3b3b004ade47f
Kyerstin O'Neal <kcrocker1@cnm.edu>
2021-04-19 16:03:23 -0600

    created DebugPlayerControls script

---
+++ Assets/Scripts/DebugPlayerControls.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugPlayerControls : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        // If the left mouse button has been pressed
+            // Convert the mouse screen position to a world ray
+            // Raycast into the world 
+                // Set the player's destinatoin to the point we clicked
+    }
+}
