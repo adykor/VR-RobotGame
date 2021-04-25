17bf5f049aefea3b6736cdb7f9b5adc6decb5f0c
Kyerstin O'Neal <kcrocker1@cnm.edu>
2021-04-25 16:25:15 -0600

    created game manager w/ singleton

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static GameManager instance; // can be called singleton
+    public Artifact[] artifacts;
+
+    private void Awake()
     {
-        
+        // Are there any other game managers yet?
+        if(instance != null)
+        {
+            // Error
+            Debug.LogError("There was more than 1 GameManager");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    internal static void OnArtifactStashed()
+    {
+
+        // Loop through all the artifacts
+            // If all artifacts are stashed
+                // You win!
     }
 }
